3a25d380c001acb7609c8bb7c687b063d33f8747
Hiiski <henriqueslima09@gmail.com>
2023-09-19 02:07:11 -0300

    first commit

---
+++ quiz/questions.js
@@ -0,0 +1,57 @@
+export default [
+    {
+        question: "Qual é o nome do planeta mais próximo do Sol no sistema solar?",
+        answers: [
+            {option: "Marte", correct: false},
+            {option: "Vênus", correct: false},
+            {option: "Mercúrio", correct: true},
+            {option: "Júpiter", correct: false},
+        ]
+    },
+    {
+        question: "Qual é o maior planeta do sistema solar?",
+        answers: [
+            {option: "Júpiter", correct: true},
+            {option: "Marte", correct: false},
+            {option: "Saturno", correct: false},
+            {option: "Netuno", correct: false},
+        ]
+    },
+    {
+        question: "Qual é a estrela central do sistema solar?",
+        answers: [
+            {option: "Lua", correct: false},
+            {option: "Vênus", correct: false},
+            {option: "Marte", correct: false},
+            {option: "Sol", correct: true},
+        ]
+    },
+    {
+        question: "Qual é a lua mais próxima da Terra?",
+        answers: [
+            {option: "Titã", correct: false},
+            {option: "Io", correct: false},
+            {option: "Lua", correct: true},
+            {option: "Europa", correct: false},
+        ]
+    },
+    {
+        question: "Qual é o planeta conhecido por seus anéis impressionantes?",
+        answers: [
+            {option: "Júpiter", correct: false},
+            {option: "Saturno", correct: true},
+            {option: "Urano", correct: false},
+            {option: "Netuno", correct: false},
+        ]
+    },
+    {
+        question: "Qual é o planeta que tem uma cor avermelhada e é frequentemente chamado de 'Planeta Vermelho'?",
+        answers: [
+            {option: "Saturno   ", correct: false},
+            {option: "Júpiter", correct: false},
+            {option: "Terra", correct: false},
+            {option: "Marte", correct: true},
+        ]
+    },
+
+]
